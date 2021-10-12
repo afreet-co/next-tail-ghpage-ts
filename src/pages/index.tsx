f486af75959c2b5c058b089c02b54f53fcd78409
Afreet Co <afreet.co20@gmail.com>
2021-10-12 15:33:57 +0530

    Added sample context to have to state management easily

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
+import { useAppContext } from "../contexts/AppContext";
+
 const Index = () => {
+  const { content, title } = useAppContext();
   return (
     <div className="flex items-center justify-center h-screen w-screen">
       <div className="shadow-2xl rounded-lg bg-gray-300 border-gray-500 border-2 w-72 p-4 space-y-4">
-        <h1 className="text-xl font-bold text-center">
-          Next.js - TailwindCSS - Github Pages - Typescript
-        </h1>
-        <p className="px-4 text-center">
-          A project to be used as a starter for Next.js project which includes
-          tailwind, it's configuration, typescript and also the github action
-          workflow deployment to Github pages after pushing commit to the master
-          branch.
-        </p>
+        <h1 className="text-xl font-bold text-center">{title}</h1>
+        <p className="px-4 text-center">{content}</p>
       </div>
     </div>
   );
